b3bdf185c651e54d0acd946bde6804a6d89526df
TeamDman <TeamDman9201@gmail.com>
2020-03-03 12:11:08 -0500

    Add recipes and about page

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -11,6 +11,11 @@ const routes = [
 		component: Home,
 	},
 	{
+		path: "/about",
+		name: "About",
+		component: () => import("@/views/About.vue"),
+	},
+	{
 		path: "/recipe/:id",
 		name: "Recipe",
 		component: () =>
