deae7ac0f8afc9e06c499ac14b26dcf747056f8b
TeamDman <TeamDman9201@gmail.com>
2020-04-03 13:50:04 -0400

    Update gh pages script

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-docker-compose up -d --build
-docker exec reciplo_vue_1 npm run build
+docker exec $1 npm run build
 git add -f app/dist
 git commit -m "Update pages"
 git subtree push --prefix app/dist origin gh-pages
